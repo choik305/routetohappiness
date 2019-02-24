a3764a185d702b8b3c01dff3fd024763184e1e23
Kevin Choi <kevinyoungchoi@gmail.com>
2019-02-24 13:46:13 -0500

    route to happiness

--- React_Routes/src/index.js
+++ React_Routes/src/index.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './CSS/index.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Happy from './components/Happy';
 import Sleepy from './components/Sleepy';
 import Guilty from './components/Guilty';
 
-// your code goes here
+const NavBar = () => {
+  return(
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Happy">Happy</Link>
+        </li>
+        <li>
+          <Link to="/Sleepy">Sleepy</Link>
+        </li>
+        <li>
+          <Link to="/Guilty">Guilty</Link>
+        </li>
+      </ul>
+
+      <Route exact path="/" component={Home} />
+      <Route path="/Happy" component={Happy} />
+      <Route path="/Sleepy" component={Sleepy} />
+      <Route path="/Guilty" component={Guilty} />
+    </div>
+  </Router>
+  )
+}
+
+
 class Home extends Component{
   render(){
     return(
-      // your code goes here
+      <div>HELLO WORLD</div>
     )
   }
 }
 
-ReactDOM.render( <Home />, document.getElementById('root') );
+ReactDOM.render( <NavBar />, document.getElementById('root') );
